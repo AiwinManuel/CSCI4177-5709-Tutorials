f1d8e3f7a92695871254c27d3558a080853550b4
Aiwin Charalil Manuel <aw380590@dal.ca>
2024-02-19 23:07:22 -0400

    final touch

--- tutorial4/src/pages/LoginPage.js
+++ tutorial4/src/pages/LoginPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './LoginPage.css'; // Make sure this import is correct
+import './LoginPage.css'; 
 
 const LoginPage = () => {
   const [loginDetails, setLoginDetails] = useState({
--- tutorial4/src/pages/ProfileDetailsPage.js
+++ tutorial4/src/pages/ProfileDetailsPage.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import './ProfileDetailsPage.css'; 
+
 const ProfileDetailsPage = () => {
   const [userDetails, setUserDetails] = useState(null);
+  const [showToast, setShowToast] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
@@ -11,6 +13,7 @@ const ProfileDetailsPage = () => {
       try {
         const response = await axios.get(`https://express-t4.onrender.com/api/users/${id}`);
         setUserDetails(response.data);
+        setShowToast(true);
       } catch (error) {
         console.error("Error fetching user details:", error);
       }
@@ -24,14 +27,39 @@ const ProfileDetailsPage = () => {
 
   return (
     <div className="profile-details-container">
-      <div className="profile-details-box">
-        <img src={userDetails.picture} alt={userDetails.name} className="profile-picture" />
-        <h2>{userDetails.name}</h2>
-        <div>Email: {userDetails.email}</div>
-        <div>Company: {userDetails.company}</div>
-        <div className="about-section">About: {userDetails.about}</div>
-      </div>
-    </div>
+  <div className="detail-box">
+    <img src={userDetails.picture} alt={userDetails.name} className="profile-picture" />
+    <h2>{userDetails.name}</h2>
+    <div>Company: {userDetails.company}</div>
+    <div className="about-section">About: {userDetails.about}</div>
+  </div>
+
+  <div className="detail-box">
+    <div className="title">Contact</div>
+    <div>Email: {userDetails.email}</div>
+    <div>Phone: {userDetails.phone}</div>
+    <div>Address: {userDetails.address}</div>
+    <div>Location: {userDetails.latitude}, {userDetails.longitude}</div>
+  </div>
+
+  <div className="detail-box">
+    <div className="title">Friends</div>
+    <ul>
+      {userDetails.friends.map(friend => (
+        <li key={friend.id}>{friend.name}</li>
+      ))}
+    </ul>
+  </div>
+
+  <div className="detail-box">
+    <div className="title">Details</div>
+    <div>Age: {userDetails.age}</div>
+    <div>Eye Color: {userDetails.eyeColor}</div>
+    <div>Gender: {userDetails.gender}</div>
+    <div>Favorite Fruit: {userDetails.favoriteFruit}</div>
+  </div>
+  {showToast && <div className="toast">{userDetails.greeting}</div>}
+</div>
   );
 };
 
